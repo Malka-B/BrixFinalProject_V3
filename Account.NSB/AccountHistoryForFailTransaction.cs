0fdd2fc08b3e1426bc4f41ffd3856b801ba8f4c2
M.B <mb0583219718@gmail.com>
2020-07-28 11:19:56 +0300

    before push

---
+++ Account.NSB/AccountHistoryForFailTransaction.cs
@@ -0,0 +1,14 @@
+﻿using Messages.Events;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+namespace Account.NSB
+{
+    public class AccountHistoryForFailTransaction : IHandleMessages<TransactionFailed>
+    {
+        public Task Handle(TransactionFailed message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Account.NSB/AccountHistoryForSuccessTransaction.cs
@@ -0,0 +1,16 @@
+﻿using Messages.Events;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Account.NSB
+{
+    public class AccountHistoryForSuccessTransaction : IHandleMessages<TransactionSucceeded>
+    {
+        //private readonly 
+        public Task Handle(TransactionSucceeded message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
